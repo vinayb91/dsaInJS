a2d7758b5c71f619f44e83974058d01fe85685ff
Vinay B <bvinay9198@gmail.com>
2024-06-10 17:21:45 +0530

    stack is completed

---
+++ stack/stack.js
@@ -0,0 +1,53 @@
+class Stack {
+    constructor(){
+        this.stack = [];
+    }
+    push(data){
+        this.stack.push(data);
+    }
+
+    pop(){
+        this.stack.pop()
+    }
+    isEmpty(){
+        return this.stack.length()==null
+    }
+
+    peek(){
+        return this.stack[this.stack.length()-1];
+    }
+
+    printStack(){
+        let str = ""
+        for (let i = 0; i <this.stack.length; i++) {
+            str+= this.stack[i]+ "\n"
+            
+        }
+        return str
+    }
+
+    reverse(){
+        return this.stack.reverse()
+    }
+
+    size(){
+        return this.stack.length
+    }
+
+    clear(){
+        this.stack = []
+    }
+
+    containsElement(element){
+       return this.stack.includes(element)
+    }
+}
+
+let mystack = new Stack()
+
+mystack.push(3)
+mystack.push(4)
+mystack.push(6)
+mystack.push(31)
+mystack.pop()
+console.log(mystack.printStack());
