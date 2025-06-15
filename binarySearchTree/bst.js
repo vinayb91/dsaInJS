e68b15197fae398c0863ecc82bd616d7cdc40823
Vinay B <bvinay9198@gmail.com>
2025-06-15 20:48:37 +0530

    binary search- delete, traversal, bst,dft

--- binarySearchTree/bst.js
+++ binarySearchTree/bst.js
@@ -67,4 +67,138 @@ class BinarySearchTree {
     }
     return node
   }
+
+  inOrderTraversal(){
+    const result = []
+    this.inOrder(this.root, result)
+    // console.log("this is root", this.root)
+
+    return result
+  }
+
+  inOrder(node, result){
+    if (node !==null) {
+      this.inOrder(node.left, result)
+      result.push(node.key)
+      this.inOrder(node.right, result)
+      
+    }
+  }
+
+  preOrderTraversal(){
+    const result = []
+    this.preOrder(this.root, result)
+    // console.log("this is root", this.root)
+    return result
+  }
+
+  preOrder(node, result){
+    if (node !==null) {
+
+      result.push(node.key)
+      this.preOrder(node.left, result)
+      this.preOrder(node.right, result)
+    }
+  }
+
+  postOrderTraversal(){
+    const result = []
+    this.postOrder(this.root, result)
+    return result
+  }
+
+  postOrder(node, result){
+    if (node !==null) {
+      this.postOrder(node.left, result)
+      this.postOrder(node.right, result)
+      result.push(node.key)
+      
+    }
+  }
+
+  recursiveDFS(root){
+    if (root === null) {
+      return 
+    }
+    const leftValues = this.recursiveDFS(root.left)
+    const rightValues = this.recursiveDFS(root.right)
+
+    return [root.key, leftValues, rightValues]
+  }
+
+  DFS(){
+    const result = this.recursiveDFS(this.root)
+    // console.log("this is root", this.root)
+
+    return result
+  }
+  DFSiterative(){
+    const result = this.iterativeDFS(this.root)
+    // console.log("this is root", this.root)
+
+    return result
+  }
+
+
+  BFS(){
+    const result = this.BredthFirstSearch(this.root)
+    // console.log("this is root", this.root)
+
+    return result
+  }
+
+  BredthFirstSearch(root){
+    const values = []
+    if (root===null) {
+      return values
+    }
+    const queue = [root]
+    while (queue.length>0) {
+      const node = queue.shift()
+      values.push(node.key)
+
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+    return values
+  }
+  iterativeDFS(root){
+    const values = []
+    if (root===null) {
+      return values
+    }
+    const stack = [root]
+    while (stack.length>0) {
+      const node = stack.pop()
+      values.push(node.key)
+
+      if (node.right!==null) {
+        stack.push(node.right)
+      }
+      if (node.left!==null) {
+        stack.push(node.left)
+      }
+    }
+    return values
+  }
 }
+
+
+let mybst = new BinarySearchTree()
+
+mybst.insert(25)
+mybst.insert(20)
+mybst.insert(15)
+mybst.insert(23)
+mybst.insert(40)
+mybst.insert(50)
+
+// console.log(mybst.inOrderTraversal())
+// console.log(mybst.preOrderTraversal())
+console.log(mybst.BFS())
+
+// mybst.delete(20)
